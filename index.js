21de47ae075ca20b2974b6e1925f10017e761a79
Michel Tuijula <michel.tuijula@hotmail.com>
2021-08-20 19:00:03 +0300

    Solution for 3_6

--- index.js
+++ index.js
@@ -60,6 +60,12 @@ app.post('/api/persons', (request, response) => {
         error: 'data missing' 
       })
     }
+
+    if (persons.find(person => person.name === body.name)) {
+        return response.status(400).json({ 
+          error: 'name must be unique' 
+        })
+    }    
   
     const person = {
         id: Math.ceil(Math.random() * 10000000),
