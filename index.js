22b34099da140641ce130d6872cf5879db88a100
Michel Tuijula <michel.tuijula@hotmail.com>
2021-08-20 18:22:53 +0300

    Solution for 3_5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -49,6 +51,26 @@ app.delete('/api/persons/:id', (request, response) => {
   
     response.status(204).end()
 })
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+  
+    if (!body.name || !body.number) {
+      return response.status(400).json({ 
+        error: 'data missing' 
+      })
+    }
+  
+    const person = {
+        id: Math.ceil(Math.random() * 10000000),
+        name: body.name,
+        number: body.number,
+    }
+  
+    persons = persons.concat(person)
+  
+    response.json(person)
+  })
 
 const PORT = 3001
 app.listen(PORT, () => {
