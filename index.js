8cb0999d27934cea146a1079acceb1950969fad6
Michel Tuijula <michel.tuijula@hotmail.com>
2021-08-21 17:37:41 +0300

    Solution for 3_18

--- index.js
+++ index.js
@@ -51,18 +51,24 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
+
+    let personCount = 'NaN'
+    Person.find({}).then(persons => {
+      personCount = persons.length
+      response.send(`<p>Phonebook has info for ${personCount} people</p><p>${new Date()}</p>`)
+    })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -99,8 +105,8 @@ app.put('/api/persons/:id', (request, response, next) => {
   }
 
   Person.findByIdAndUpdate(request.params.id, person, { new: true })
-  .then(updatedNote => {
-    response.json(updatedNote)
+  .then(updatedPerson => {
+    response.json(updatedPerson)
   })
   .catch(error => next(error))
 })
